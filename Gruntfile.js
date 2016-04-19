7855b1f6e2293f9683f214c06a381d8e531356e7
Dylan Schirino <dylan.schirino@hotmail.fr>
2016-04-19 14:58:36 +0200

    Fixing bug and started responsive

--- Gruntfile.js
+++ Gruntfile.js
@@ -21,7 +21,7 @@ module.exports = function( grunt ) {
                     "src": "css/*.css"
                 },
                 "options": {
-                    "proxy": "localhost:44796", // TODO: adapt to you configuration
+                    "proxy": "localhost:55996", // TODO: adapt to you configuration
                     "watchTask": true
                 }
             }
